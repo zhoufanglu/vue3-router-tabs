2e6b9c7fb7f3dd63b84d6254670337b63cf41e0b
lufangzhou <lufangzhou@gmail.com>
2023-04-22 15:58:38 +0800

    feat: 打包文件

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,10 @@ import RouterTabs from './router-tabs/index.vue'
 // 按需引入导出
 export {RouterTabs}
 // 全局导出
-const Vue3RouterNavBar = {
+const Vue3RouterTabs = {
   install(app: any) {
     app.component('RouterTabs', RouterTabs)
   },
 }
 
-export default Vue3RouterNavBar
+export default Vue3RouterTabs
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import App from './App.vue'
 
 const app = createApp(App)
 // 引入开发环境
-import Vue3RouterNavBar from './components'
+// import Vue3RouterNavBar from './components'
 // 引入打包
 // @ts-ignore
 //vite不支持导入umd.js文件类型 所以要另外打包成mjs
-//import Vue3RouterNavBar from '../lib/vue3-router-tabs.umd.cjs'
+import Vue3RouterTabs from '../lib/vue3-router-tabs.js'
 // import Vue3RouterNavBar from '../lib/vue3-router-tabs.js'
-app.use(Vue3RouterNavBar)
+// @ts-ignore
+app.use(Vue3RouterTabs)
 
 app.mount('#app')
